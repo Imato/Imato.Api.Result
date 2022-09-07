3a57e88f9865acaa4889e55a77f507469c96f3d2
imato <alexey.varentsov@gmail.com>
2022-09-07 21:32:44 +0300

    new version

---
+++ Imato.Api.Result/ApiResult.cs
@@ -0,0 +1,75 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Imato.Api.Result
+{
+    public class ApiResult
+    {
+        /// <summary>
+        /// Successful message
+        /// </summary>
+        [JsonPropertyName("message")]
+        public string? Message { get; set; }
+
+        /// <summary>
+        /// Error message
+        /// </summary>
+        [JsonPropertyName("error")]
+        public string? Error { get; set; }
+
+        /// <summary>
+        /// Set true for return error in controller
+        /// </summary>
+        [System.Text.Json.Serialization.JsonIgnore]
+        [Newtonsoft.Json.JsonIgnore]
+        public bool IsFail { get; set; }
+
+        /// <summary>
+        /// Result object
+        /// </summary>
+        [JsonPropertyName("result")]
+        public object? Result { get; set; }
+
+        public static ApiResult Ok = new ApiResult
+        {
+            Message = "Ok"
+        };
+
+        public static ApiResult ServerError = new ApiResult
+        {
+            Error = "Unknown server error",
+            IsFail = true
+        };
+
+        public static ApiResult Fail(string message) => new ApiResult { Error = message, IsFail = true };
+
+        public static ApiResult Success(string message) => new ApiResult { Message = message };
+
+        public static ApiResult Success(object value) => new ApiResult { Result = value };
+    }
+
+    public class ApiResult<T> : ApiResult where T : class
+    {
+        /// <summary>
+        /// Result object
+        /// </summary>
+        [JsonPropertyName("result")]
+        public new T? Result { get; set; }
+
+        public static ApiResult<T> Ok = new ApiResult<T>
+        {
+            Message = "Ok"
+        };
+
+        public new static ApiResult<T> ServerError = new ApiResult<T>
+        {
+            Error = "Unknown server error",
+            IsFail = true
+        };
+
+        public new static ApiResult<T> Fail(string message) => new ApiResult<T> { Error = message, IsFail = true };
+
+        public new static ApiResult<T> Success(string message) => new ApiResult<T> { Message = message };
+
+        public static ApiResult<T> Success(T value) => new ApiResult<T> { Result = value };
+    }
+}
---
+++ Imato.Api.Result/ApiResultExtensions.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Imato.Api.Result
+{
+    public static class ApiResultExtensions
+    {
+        private static MediaTypeCollection contentTypes = new MediaTypeCollection { Application.Json };
+
+        /// <summary>
+        /// Add resutl
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="result"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static ApiResult<T> Result<T>(this ApiResult<T> result, T value) where T : class
+        {
+            result.Result = value;
+            return result;
+        }
+
+        /// <summary>
+        /// Add result
+        /// </summary>
+        /// <param name="result"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static ApiResult Result(this ApiResult result, object value)
+        {
+            result.Result = value;
+            return result;
+        }
+
+        /// <summary>
+        /// Add error message
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="result"></param>
+        /// <param name="error"></param>
+        /// <returns></returns>
+        public static ApiResult<T> Fail<T>(this ApiResult<T> result, string error) where T : class
+        {
+            result.Error = error;
+            result.IsFail = true;
+            return result;
+        }
+
+        /// <summary>
+        /// Add error message
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="result"></param>
+        /// <param name="error"></param>
+        /// <returns></returns>
+        public static ApiResult Fail(this ApiResult result, string error)
+        {
+            result.Error = error;
+            result.IsFail = true;
+            return result;
+        }
+
+        /// <summary>
+        /// Add message
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="result"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static ApiResult<T> Success<T>(this ApiResult<T> result, string message) where T : class
+        {
+            result.Message = message;
+            return result;
+        }
+
+        /// <summary>
+        /// Add message
+        /// </summary>
+        /// <param name="result"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static ApiResult Success(this ApiResult result, string message)
+        {
+            result.Message = message;
+            return result;
+        }
+
+        /// <summary>
+        /// Add result
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="result"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static ApiResult<T> Success<T>(this ApiResult<T> result, T value) where T : class
+        {
+            result.Result = value;
+            return result;
+        }
+
+        /// <summary>
+        /// Add result
+        /// </summary>
+        /// <param name="result"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static ApiResult Success(this ApiResult result, object value)
+        {
+            result.Result = value;
+            return result;
+        }
+
+        /// <summary>
+        /// Get result for controller action
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public static ActionResult<T> ActionResult<T>(this ApiResult<T> result) where T : class
+        {
+            if (result == null)
+            {
+                return ApiResult<T>.ServerError.ActionResult();
+            }
+
+            if (result.IsFail)
+            {
+                return new BadRequestObjectResult(result);
+            }
+
+            return new OkObjectResult(result);
+        }
+
+        /// <summary>
+        /// Get result for controller action
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public static ActionResult ActionResult(this ApiResult result)
+        {
+            if (result == null)
+            {
+                return new BadRequestObjectResult(ApiResult.ServerError)
+                {
+                    ContentTypes = contentTypes
+                };
+            }
+
+            if (result.IsFail)
+            {
+                return new BadRequestObjectResult(result)
+                {
+                    ContentTypes = contentTypes
+                };
+            }
+
+            return new OkObjectResult(result)
+            {
+                ContentTypes = contentTypes
+            };
+        }
+    }
+}
